193b89e859b98cfdabca6c8b4c83c94538f58590
Nguyen Ngoc Vu <vudev98@gmail.com>
2022-10-19 07:18:20 +0700

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import * as React from "react"
+import { useState, useEffect } from 'react'
 import './styles.css'
 
 export const App = () => {
+  const [productList, setProductList] = useState(
+    useEffect(() => {
+      fetch('https://fakestoreapi.com/products')
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+    }, [])
+  )
   return <h1>React TypeScript</h1>
 }
